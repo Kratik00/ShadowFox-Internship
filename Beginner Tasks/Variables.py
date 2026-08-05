89d5027c4d56790f2a0ffbd74277485c0c9fa914
kratik <kratik437@gmail.com>
2026-08-05 11:36:42 +0530

    first task

---
+++ Beginner Tasks/Variables.py
@@ -0,0 +1,25 @@
+#===========================================================
+# Task-01: Variables
+# Question 01
+pi = 22 / 7
+print(f"Value of Pi: {pi:.4f}")
+print(f"Data Type: {type(pi)}")
+
+#Question 02
+# Uncomment 8th line to see syntax error.
+# for = 4
+
+# Explanation:
+# 'For' is a reserved keyword in python thats why its giving syntax error.
+
+# Question 03
+principal_amount = 200000
+rate_of_interest = 6   
+time = 3 
+simple_interest = (principal_amount * rate_of_interest * time) / 100
+print(f"Principal Amount: {principal_amount}")
+print(f"Rate of Interest: {rate_of_interest}%")
+print(f"Time: {time} Years")
+print(f"Simple Interest: {simple_interest:.2f}")
+
+#===========================================================
